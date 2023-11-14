d0c6c81b4f434730ad77ed98b98bbaeb7848481b
OrlaMarco <148097286+OrlaMarco@users.noreply.github.com>
2023-11-14 10:09:05 +0100

    Add files via upload

---
+++ autoDeploy/src/main/java/it/syncroweb/autoDeploy/AutoDeployApplication.java
@@ -0,0 +1,13 @@
+package it.syncroweb.autoDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDeployApplication.class, args);
+	}
+
+}
